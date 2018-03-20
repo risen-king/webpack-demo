f44eedd742eb5e3ae5324a234c59c70a2bc2854a
risen <ahcj_11@163.com>
2018-03-20 20:42:09 +0800

    添加表单验证功能

--- src/components/UserAdd.js
+++ src/components/UserAdd.js
@@ -4,10 +4,24 @@ class UserAdd extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            name: '',
-            age: 0,
-            gender: ''
-        }
+            form: {
+                name: {
+                    valid: false,
+                    value: '',
+                    error: ''
+                },
+                age: {
+                    valid: false,
+                    value: 0,
+                    error: ''
+                },
+                gender: {
+                    valid: false,
+                    value: '',
+                    error: ''
+                }
+            }
+        };
     }
 
     handleValueChange (field, value, type = 'string') {
@@ -16,8 +30,42 @@ class UserAdd extends React.Component {
             value = +value;
         }
 
+        const {form} = this.state;
+
+        const newFieldObj = {value, valid: true, error: ''};
+        switch (field) {
+            case 'name': {
+                if (value.length >= 5) {
+                    newFieldObj.error = '用户名最多4个字符';
+                    newFieldObj.valid = false;
+                } else if (value.length === 0) {
+                    newFieldObj.error = '请输入用户名';
+                    newFieldObj.valid = false;
+                }
+                break;
+            }
+            case 'age': {
+                if (value > 100 || value <= 0) {
+                    newFieldObj.error = '请输入1~100之间的数字';
+                    newFieldObj.valid = false;
+                }
+                break;
+            }
+            case 'gender': {
+                if (!value) {
+                    newFieldObj.error = '请选择性别';
+                    newFieldObj.valid = false;
+                }
+                break;
+            }
+        }
+
+
         this.setState({
-            [field]: value
+            form: {
+                ...form,
+                [field]: newFieldObj
+            }
         });
     }
     handleSubmit (e) {
@@ -25,15 +73,20 @@ class UserAdd extends React.Component {
         e.preventDefault();
         //alert(JSON.stringify(this.state));
 
-        const {name, age, gender} = this.state;
+        const {form: {name, age, gender}} = this.state;
+
+        if (!name.valid || !age.valid || !gender.valid) {
+            alert('请填写正确的信息后重试');
+            return;
+        }
 
         fetch('http://localhost:3000/user', {
                     method: 'post',
                     // 使用fetch提交的json数据需要使用JSON.stringify转换为字符串
                     body: JSON.stringify({
-                        name,
-                        age,
-                        gender
+                        name: name.value,
+                        age: age.value,
+                        gender: gender.value
                     }),
                     headers: {
                         'Content-Type': 'application/json'
@@ -60,7 +113,7 @@ class UserAdd extends React.Component {
     }
 
     render(){
-        const {name, age, gender} = this.state;
+        const {form: {name, age, gender}} = this.state;
 
         return (
             <div>
@@ -71,17 +124,31 @@ class UserAdd extends React.Component {
                 <main>
                     <form onSubmit={(e) => this.handleSubmit(e)}>
                         <label>用户名：</label>
-                        <input type="text" value={name} onChange={(e) => this.handleValueChange('name', e.target.value)}/>
+                        <input
+                            type="text"
+                            value={name.value}
+                            onChange={(e) => this.handleValueChange('name', e.target.value)}
+                        />
+                        {!name.valid && <span>{name.error}</span>}
                         <br/>
                         <label>年龄：</label>
-                        <input type="number" value={age || ''} onChange={(e) => this.handleValueChange('age', e.target.value, 'number')}/>
+                        <input
+                            type="number"
+                            value={age.value || ''}
+                            onChange={(e) => this.handleValueChange('age', e.target.value, 'number')}
+                        />
+                        {!age.valid && <span>{age.error}</span>}
                         <br/>
                         <label>性别：</label>
-                        <select  value={gender}  onChange={(e) => this.handleValueChange('gender', e.target.value)}>
-                            <option value="">请选择</option>
-                            <option value="male">男</option>
-                            <option value="female">女</option>
+                        <select
+                            value={gender.value}
+                            onChange={(e) => this.handleValueChange('gender', e.target.value)}
+                        >
+                                <option value="">请选择</option>
+                                <option value="male">男</option>
+                                <option value="female">女</option>
                         </select>
+                        {!gender.valid && <span>{gender.error}</span>}
                         <br/>
                         <br/>
                         <input type="submit" value="提交"/>
